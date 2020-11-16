887e347edbae877d1c6032799131c0c3c9c2e867
David Spruill <daspr@microsoft.com>
2020-11-16 08:05:43 +0000

    Merged PR 5408660: Add the display control prototype
    Add the display control prototype

---
+++ Prototypes/DisplayControl/DDisplay/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Windows.h>
+#include <dxgi1_6.h>
+#include <d3d11_4.h>
+#include <d2d1_3.h>
+#include <d2d1_3helper.h>
+#include <wrl.h>
+
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <functional>
+
+#include <d3dkmthk.h>
