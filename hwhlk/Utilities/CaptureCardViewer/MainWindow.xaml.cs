6ca43703a328fd96683883c061d586c01e0063c8
Larissa Umulinga <lumulinga@microsoft.com>
2022-05-11 12:21:52 -0700

    Displaying generated and captured frames

--- hwhlk/Utilities/CaptureCardViewer/MainWindow.xaml.cs
+++ hwhlk/Utilities/CaptureCardViewer/MainWindow.xaml.cs
@@ -68,86 +68,90 @@ namespace CaptureCardViewer
             InitializeComponent();
         }
 
-        private void LoadPlugin_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new OpenFileDialog();
-            dialog.Filter = "Plugin DLLs|*.dll";
-            dialog.Title = "Load a Capture Plugin";
-			 
-
-            if (dialog.ShowDialog() ?? false)
-            {
-                try
-                {
-					//Framework loading the components
-                    testFramework = new Core();
-					var cwd=System.IO.Directory.GetCurrentDirectory();
-					string fullPath = (cwd.ToString() + "BasicConfig.json").ToString();
-					testFramework.LoadConfigFile(fullPath);
-					testFramework.LoadPlugin(dialog.FileName, "GenericCaptureCardPlugin.Plugin");
-					testFramework.LoadToolbox(dialog.FileName, "DisplayConfiguration.Toolbox");
-					testFramework.LoadDisplayManager(dialog.FileName, "DisplayControl.DisplayEngine");
-					
-				}
-                catch (Exception)
-                {
-
-                }
-            }
-        }
-
-		//sets the tool from the selected item in the ComboBox
-		private void availableTools_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{	
-			ComboBoxItem cbi = (ComboBoxItem)availableTools.SelectedItem;	
-			string? selected = cbi.Content.ToString();
-			switch (selected)
-			{
-				case "Event Log":
-					setTool = "EventLog";
-					break;
-
-				case "Supported Features":
-					setTool = "SuppFeat";
-					break;
-
-				case "":
-					setTool = "LoadFromDisk";
-					break;
-
-			}
-		}
-
-		//sets the current tool for the add button
-		private void addButtonClick(object sender, RoutedEventArgs e)
+		//Loading the plugin framework
+		private async void loadFramework (object sender, RoutedEventArgs e)
 		{
-			currentTool = setTool;
-			 
-		} 
-
-		//getting predicted frames to frame comparison
-		private async void displayPredictedFrames(IDisplayEngine displayEngine,IDisplayInput captureInput)
-		{
-
+			/*var dialog = new OpenFileDialog(); //file picker
+			dialog.Filter = "Plugin DLLs|*.dll";
+			dialog.Title = "Load a Capture Plugin";
+			string configPath = "TestConfig.json";*/
+			String configPath= "TestConfig.json";
 			await Task.Run(() =>
 			{
-				System.Windows.Media.Imaging.BitmapSource pSource;
+				testFramework.LoadConfigFile(configPath);
+			});
 
+		}
+
+		//Displaying frames & properties from the plugin
+		private async void displayProperties(object sender, RoutedEventArgs e)
+		{
+			// image sources for generated & captured frames 
+			System.Windows.Media.Imaging.BitmapSource bSource;
+			System.Windows.Media.Imaging.BitmapSource pSource;
+			
+			//Display & Capture of frames 
+			await Task.Run(() =>
+			{
+				//Captured frames from the tanager board 
+				var genericCapture = testFramework.GetCaptureCard();
+				var captureInputs = genericCapture.EnumerateDisplayInputs();
+				var displayEngine = testFramework.GetDisplayEngine();
+				var captureInput = captureInputs[0];
+				captureInput.FinalizeDisplayState();
+				var capturedFrame = captureInput.CaptureFrame();
+				var pixelBuffer = capturedFrame.GetRawPixelData();
+
+				// This needs to be inside of an unsafe block because we are manipulating bytes directly
+				unsafe
+				{
+
+					byte[] bytes = new byte[pixelBuffer.Capacity];
+					fixed (byte* bytesAccess = bytes)
+					{
+						byte* ptr;
+						uint capacity;
+						var ByteAccess = pixelBuffer.As<IMemoryBufferByteAccess>();
+						ByteAccess.GetBuffer(out ptr, out capacity);
+
+						// copy the raw memory out to the byte array
+						Unsafe.CopyBlockUnaligned(
+							ref bytesAccess[0], ref ptr[0], capacity);
+
+					}
+					var pixCap = pixelBuffer.Capacity;
+					bSource = System.Windows.Media.Imaging.BitmapSource.Create(800, 600, 96, 96, PixelFormats.Bgr32, null, bytes, (800 * PixelFormats.Bgr32.BitsPerPixel + 7) / 8);
+
+				}
+
+				bSource.Freeze();
+				// You can't update UI elements from a background thread (here in the Await Task.Run). So we update the UI by queuing up an operation on the UI thread
+				this.Dispatcher.Invoke(
+						new Action(() =>
+						{
+							ActualImage.Source = bSource;
+						}
+						));
+
+				Thread.Sleep(1000);
+
+				//Loading & displaying tools
 				//Reset the display manager to the correct one
 				displayEngine.InitializeForStableMonitorId("DEL41846VTHZ13_1E_07E4_EC");
 
 				// Get the list of tools, iterate through it and call 'apply' without changing the default setting
 				var tools = testFramework.GetLoadedTools();
-				
+
 				foreach (var tool in tools)
-				
 					tool.Apply(displayEngine);
 
 				var renderer = displayEngine.StartRender();
 
 				Thread.Sleep(5000);
 
-				var capturedFrame = captureInput.CaptureFrame();
+				//TODO: Add Textbox displaying plugin tools ia string form
+
+				//Generate  & display frames to compare the Tanager's frames against
 				renderer.Dispose();
 				var prediction = displayEngine.GetPrediction();
 				var bitmap = prediction.GetBitmap();
@@ -183,113 +187,32 @@ namespace CaptureCardViewer
 					}
 				));
 				Thread.Sleep(1000);
+				
+
 			});
+		
 		}
 
-		//run the frame debugger depending on the set tool
-		private async void runAndCompareButtonClick(object sender, RoutedEventArgs e)
-		{
-			// Eventually the config file should be sourced from a file picker like you have below here in comments.
-			/*var dialog = new OpenFileDialog(); //file picker
-			dialog.Filter = "Plugin DLLs|*.dll";
-			dialog.Title = "Load a Capture Plugin";*/
-			string fP = "TestConfig.json";
-			
-			switch (currentTool)
+		//sets the tool from the selected item in the ComboBox
+		private void availableTools_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{	
+			ComboBoxItem cbi = (ComboBoxItem)availableTools.SelectedItem;	
+			string? selected = cbi.Content.ToString();
+			switch (selected)
 			{
-				case (null or "LoadFromDisk"):
-					await Task.Run(() =>
-					{
-						//testFramework.LoadConfigFile(fP);
-					});
-
-					MessageBox.Show("Capture card plugin done loading");
-
+				case "Event Log":
+					setTool = "EventLog";
 					break;
 
-				case "EventLog":
-					//TODO: Query event logs from the capture card plugin
+				case "Supported Features":
+					setTool = "SuppFeat";
 					break;
 
-				case "SuppFeat":
-					testFramework.GetLoadedTools();
+				case "":
+					setTool = "LoadFromDisk";
 					break;
+
 			}
-
-			System.Windows.Media.Imaging.BitmapSource bSource;
-			
-			//capturing frames 
-			await Task.Run(() =>
-			{
-				testFramework.LoadConfigFile(fP);
-				var genericCapture = testFramework.GetCaptureCard();
-				var captureInputs = genericCapture.EnumerateDisplayInputs();
-				var displayEngine = testFramework.GetDisplayEngine();
-				var captureInput = captureInputs[0];
-				captureInput.FinalizeDisplayState();
-
-				displayPredictedFrames(displayEngine, captureInput);
-
-				/*//Reset the display manager to the correct one
-				displayEngine.InitializeForStableMonitorId("DEL41846VTHZ13_1E_07E4_EC");
-
-				// Get the list of tools, iterate through it and call 'apply' without changing the default setting
-				var tools = testFramework.GetLoadedTools();
-				foreach (var tool in tools)
-					tool.Apply(displayEngine);
-
-				var renderer = displayEngine.StartRender();
-
-				Thread.Sleep(5000);
-
-				var capturedFrame = captureInput.CaptureFrame();
-
-				renderer.Dispose();
-
-				var prediction = displayEngine.GetPrediction();*/
-				var capturedFrame = captureInput.CaptureFrame();
-				var pixelBuffer = capturedFrame.GetRawPixelData();
-				
-				// This needs to be inside of an unsafe block because we are manipulating bytes directly
-				unsafe
-				{
-					
-					byte[] bytes = new byte[pixelBuffer.Capacity];
-					fixed (byte* bytesAccess = bytes)
-					{
-						byte* ptr;
-						uint capacity;
-						var ByteAccess = pixelBuffer.As<IMemoryBufferByteAccess>();
-						ByteAccess.GetBuffer(out ptr, out capacity);
-
-						// copy the raw memory out to the byte array
-						Unsafe.CopyBlockUnaligned(
-							ref bytesAccess[0], ref ptr[0], capacity);
-						
-					}
-					var pixCap = pixelBuffer.Capacity;
-
-					bSource = System.Windows.Media.Imaging.BitmapSource.Create(800, 600, 96, 96, PixelFormats.Bgr32, null, bytes, (800 * PixelFormats.Bgr32.BitsPerPixel + 7) / 8);
-					
-				}
-
-				//
-				bSource.Freeze();
-			// You can't update UI elements from a background thread (here in the Await Task.Run). So we update the UI by queuing up an operation on the UI thread
-			this.Dispatcher.Invoke(
-					new Action(() => 
-					{
-						ActualImage.Source = bSource;	
-					}
-					));
-				
-				Thread.Sleep(1000);
-					
-				
-			});
-
-
-
-
 		}
+
     }}
